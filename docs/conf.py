4c23f6fe067bddcc09ec038a7294939bb8c5c5d7
Manuel Garcia <gilbert.mg22@gmail.com>
2024-07-31 14:57:27 +0200

    set up documentation using Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Illuminator'
+copyright = '2024, Illuminator Team'
+author = 'Illuminator Team'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser",
+              'sphinx_rtd_theme',
+              'sphinx.ext.autodoc',
+              ]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/user/start.md
@@ -0,0 +1,3 @@
+# User Documentation
+
+> Comming soon...
