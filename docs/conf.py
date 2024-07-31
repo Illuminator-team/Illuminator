8cd2e410ea3a869e1fb82e7570932e5495aa5221
Manuel Garcia <gilbert.mg22@gmail.com>
2024-07-31 15:00:11 +0200

    chore: Add sphinx-copybutton and sphinx-rtd-theme to documentation requirements

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ release = '1.0.0'
 extensions = ["myst_parser",
               'sphinx_rtd_theme',
               'sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx-copybutton',
               ]
 
 templates_path = ['_templates']
