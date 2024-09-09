70b0f2911618f6fcdad91233c1a198b38bfa5ea0
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-09 11:00:24 +0200

    Update documentation configuration and add custom CSS for logo width in side navigation search

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
 html_logo = "_static/img/illuminator.jpg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
+    'collapse_navigation': True,
 }
+
