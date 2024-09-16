7c1f6a9eeb668fc33fc6c0e7d4c1679177bfd1e5
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-16 17:09:13 +0200

    update modeling schema

--- configs/modelling_schema.py
+++ configs/modelling_schema.py
@@ -18,10 +18,10 @@ schema = Schema( # a mapping of mappings
                     "end_time": And(Use(int), lambda n: n > 0),
                     }
         ),
-        "nodes": Schema( # a sequence of mappings
+        "models": Schema( # a sequence of mappings
             [ {
                 "name": And(str, len),
-                "model": And(str, len),
+                "type": And(str, len),
                 "inputs": And(list, lambda l: all(isinstance(x, str) for x in l)),
                 "outputs": And(list, lambda l: all(isinstance(x, str) for x in l)),
                 "parameters": And(list, lambda l: all(isinstance(x, str) for x in l)),
@@ -34,8 +34,26 @@ schema = Schema( # a mapping of mappings
                 "to": And(str, len),
                 "data": And(list, lambda l: all(And(isinstance(x, tuple), len(x) == 2 ) for x in l)),
             }]
+        ),
+        "monitor":  Schema( # a sequence of mappings
+            [{
+                "name": And(str, len),
+                Optional("inputs"):  And(list, lambda l: all(And(isinstance(x, str)) for x in l)),
+                Optional("outputs"):  And(list, lambda l: all(And(isinstance(x, str)) for x in l)),
+                Optional("states"): And(list, lambda l: all(And(isinstance(x, str)) for x in l)),
+            }]
         )
     }
 )
 
 
+# validades monitor contains at least one set of inputs, outputs, or states
+def at_least_one_set(d):
+    return any(d.get(key) for key in ["inputs", "outputs", "states"])
+
+def validate_monitor(data):
+    for item in data.get("monitor", []):
+        print(item)
+        if not at_least_one_set(item):
+            raise SchemaError("At least one of 'inputs', 'outputs', or 'states' must be set in each monitor item.")
+    return True
