34a7276b9051afdd16179d0bf00fa27f264988ea
Manuel Garcia <gilbert.mg22@gmail.com>
2024-09-30 17:17:20 +0200

    add: create Adder model class in model_example.py

---
+++ src/illuminator/builder/model_example.py
@@ -0,0 +1,13 @@
+"""
+This defines a new model
+"""
+from typing import Any
+from illuminator.builder.model_constructor import ModelConstructor
+
+class Adder(ModelConstructor):
+    """
+    A simple adder model
+    """
+
+    def model_parameters(self):
+       
