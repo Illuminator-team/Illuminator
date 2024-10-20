40e217fd9d12a5e7ab71251ab560925b4130d3b1
Manuel Garcia <gilbert.mg22@gmail.com>
2024-10-21 00:08:19 +0200

    refactor: clean up IlluminatorModel class and improve docstrings

--- src/illuminator/builder/model.py
+++ src/illuminator/builder/model.py
@@ -1,22 +1,25 @@
 from abc import ABC, abstractmethod
-from typing import Dict, List, Any, Optional
+from typing import Dict, List, Optional
 from dataclasses import dataclass, field
 from enum import Enum
+
 
 class SimulatorType(Enum):
     TIME_BASED = 'time-based'
     EVENT_BASED = 'event-based'
     HYBRID = 'hybrid'
 
+
 @dataclass
 class IlluminatorModel():
     """ A dataclass for defining the properties of a model in the Illuminator.
-    
+
     Attributes
     ----------
 
     parameters: dict
-        Properties of the object being modeled, e.g. material the object is made of
+        Properties of the object being modeled, e.g. material the object is 
+        made of.
     inputs: dict
         One or more name:value pairs to be regarded as inputs to the model.
         Inputs allow users to connect a model to other models.
@@ -24,11 +27,12 @@ class IlluminatorModel():
         One or more name:value pairs to be regarded as outputs of the model.
         Outputs allow users to connect a model to other models.
     states: dict
-        One or more 'parameters, inputs or outputs' and their initial value that are of 
-        interest for monitoring or logging during a simulation.
+        One or more 'parameters, inputs or outputs' and their initial value
+        that are of interest for monitoring or logging during a simulation.
     triggers: list
-        A flag [input's or output's name] that causes the `step` method to be called.
-        Only relevant when a model is part of an event-based or hybrid simulator.
+        A flag [input's or output's name] that causes the `step` method to
+        be called.Only relevant when a model is part of an event-based or
+        hybrid simulator.
     simulator_type: SimulatorType
         The type of simulator that the model belongs to.
     time_step_size: int
@@ -45,7 +49,6 @@ class IlluminatorModel():
     simulator_type: SimulatorType = SimulatorType.TIME_BASED
     time_step_size: int = 1
 
-
     def __post_init__(self):
         self.validate_states()
         self.validate_triggers()
@@ -54,21 +57,28 @@ class IlluminatorModel():
     def validate_states(self):
         """Check if items in 'states' are in parameters, inputs or outputs"""
         for state in self.states:
-            if state not in self.parameters and state not in self.inputs and state not in self.outputs:
-                raise ValueError(f"State: {state} must be either a parameter, an inputs or an output")
-    
+            if state not in self.parameters and state not in self.inputs and \
+                    state not in self.outputs:
+                raise ValueError(f"State: {state} must be either a parameter, "
+                                 "an input or an output")
+
     def validate_triggers(self):
         """Check if triggers are in inputs or outputs"""
         for trigger in self.triggers:
             if trigger not in self.inputs and trigger not in self.outputs:
-                raise ValueError(f"Trigger: {trigger} must be either an input or an output")
-    
+                raise ValueError(f"Trigger: {trigger} must be either an input "
+                                 "or an output")
+
     def validate_simulator_type(self):
-        """Check if triggers are defined for time-based and event-based simulations only"""
+        """Check if triggers are defined for time-based and event-based
+        simulations only
+        """
         if self.simulator_type == SimulatorType.TIME_BASED and self.triggers:
-            raise ValueError("Triggers are not allowed in time-based simulators")
-            
-        if self.simulator_type == SimulatorType.EVENT_BASED and not self.triggers:
+            raise ValueError("Triggers are not allowed in time-based "
+                             "simulators")
+
+        if self.simulator_type == SimulatorType.EVENT_BASED and \
+                not self.triggers:
             raise ValueError("Triggers are required in event-based simulators")
 
 
@@ -80,7 +90,9 @@ class ModelConstructor(ABC):
 
     @abstractmethod
     def step(self) -> None:
-        """Defines the computations that need to be performed in each simulation step"""
+        """Defines the computations that need to be performed in each
+        simulation step
+        """
         pass
 
     def current_time(self):
@@ -91,30 +103,4 @@ class ModelConstructor(ABC):
 
 if __name__ == "__main__":
 
-    # usage
-    print("Modeling a battery")
-
-    # step 1: defin model properties
-    battery = IlluminatorModel(
-        parameters={"material": "lithium"},
-        inputs={"voltage":60},
-        outputs={"voltage": 20},
-        states={"voltage": 10}
-    )
-
-
-    # step 2: create a model by inheriting from ModelConstructor
-    # and implementing the step method
-    class BatteryModel(ModelConstructor):
-
-        def step(self) -> None:
-            return self._model.inputs["voltage"] - self._model.outputs["voltage"]
-    
-    battery_model = BatteryModel(battery)
-
-    # step 3: properties can be overriden
-    battery.inputs["voltage"] = 300
-    
-    # step 4: run the model
-    print(battery_model.step())
     pass
