194331bf2b4ea0053a9ab6b5d2f870dbf5cec9f9
Manuel Garcia <gilbert.mg22@gmail.com>
2024-10-21 00:36:40 +0200

    Refactor validation messages and improve code formatting in simulation schema

--- src/illuminator/schemas/simulation.py
+++ src/illuminator/schemas/simulation.py
@@ -14,8 +14,10 @@ valid_start_time = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$'
 ipv4_pattern = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 ipv6_pattern = r'^(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}$'
 valid_ip = f'({ipv4_pattern})|({ipv6_pattern})'
-# monitor and connections sections enforce a format such as <model>.<input/output/state>
+# monitor and connections sections enforce a format such as 
+# <model>.<input/output/state>
 valid_model_item_format = r'^\w+\.\w+$'
+
 
 def validate_model_item_format(items: list) -> list:
     """
@@ -25,8 +27,10 @@ def validate_model_item_format(items: list) -> list:
     pattern = re.compile(valid_model_item_format)
     for item in items:
         if not pattern.match(item):
-            raise SchemaError(f"Invalid format for monitor item: {item}. Must be in the format: <model>.<item>")
+            raise SchemaError(f"Invalid format for monitor item: {item}. "
+                              "Must be in the format: <model>.<item>")
     return items
+
 
 def validate_file_path(file_path: str) -> str:
     """
@@ -36,47 +40,66 @@ def validate_file_path(file_path: str) -> str:
         raise SchemaError(f"File path does not exist: {file_path}")
     return file_path
 
+
 class ScenarioSchema(Schema):
     """
-    A schema for validating the scenario section of the simulation configuration file.
+    A schema for validating the scenario section of the simulation
+    configuration file.
     """
-    def validate(self, data, _is_scenario_schema=True): # _is_scenario_schema is a flag to limit validation of the scenario schema
-        data = super(ScenarioSchema, self).validate(data, _is_scenario_schema=False)
+    def validate(self, data, _is_scenario_schema=True):  # _is_scenario_schema
+        # is a flag to limit validation of the scenario schema
+        data = super(ScenarioSchema, self).validate(data,
+                                                    _is_scenario_schema=False)
         
-        if _is_scenario_schema and data.get("start_time", None) and data.get("end_time", None):
+        if _is_scenario_schema and data.get("start_time", None) and \
+                data.get("end_time", None):
             # convert strings to datetime objects
-            start_time_ = datetime.datetime.strptime(data["start_time"], "%Y-%m-%d %H:%M:%S")
-            end_time_ = datetime.datetime.strptime(data["end_time"], "%Y-%m-%d %H:%M:%S")
+            start_time_ = datetime.datetime.strptime(data["start_time"],
+                                                     "%Y-%m-%d %H:%M:%S")
+            end_time_ = datetime.datetime.strptime(data["end_time"],
+                                                   "%Y-%m-%d %H:%M:%S")
             if start_time_ >= end_time_:
-                raise SchemaError("End time cannot be less than or equal to the start time.")
+                raise SchemaError("End time cannot be less than or equal to "
+                                  "the start time.")
         return data
-    
+
+
 # Define the schema for the simulation configuration file
-schema = Schema( # a mapping of mappings
+schema = Schema(  # a mapping of mappings
             {
                 "scenario": ScenarioSchema(
                     {
-                    "name": And(str, len),
-                    "start_time": Regex(valid_start_time, error="Invalid start_time format. Must be in the format: YYYY-MM-DDTHH:MM:SS"),
-                    "end_time": Regex(valid_start_time, error="Invalid end_time format. Must be in the format: YYYY-MM-DDTHH:MM:SS"),
+                        "name": And(str, len),
+                        "start_time": Regex(valid_start_time, error="Invalid "
+                                            "start_time format. Must be in "
+                                            "the format: YYYY-MM-DDTHH:MM:SS"),
+                        "end_time": Regex(valid_start_time, error="Invalid "
+                                          "end_time format. Must be in the"
+                                          "format: YYYY-MM-DDTHH:MM:SS"),
                     }
-        ),
-        "models": Schema( # a sequence of mappings
-            [ {
-                "name": And(str, len),
-                "type": And(str, len),
-                "inputs": And(dict, len, error="if 'inputs' is used, it must contain at least one key-value pair"),
-                "outputs": And(dict, len, error="if 'outputs' is used, it must contain at least one key-value pair"),
-                Optional("parameters"): And(dict, len, error="if 'parameters' is used, it must contain at least one key-value pair"),
-                Optional("states"): And(dict, len, error="if 'states' is used, it must contain at least one key-value pair"),
-                Optional("triggers"): And(list, len, error="if 'trigger' is used, it must contain at least one key-value pair"),
-                Optional("scenario_data"): And(str, 
-                                               Use(validate_file_path, error=f"'scenario_data' points to a file that doesn't exist"), 
-                                               error="If 'scenario_data' is used, a valid file path must be provided"), 
-                Optional("connect"): Schema( # a mapping of mappings
-                    {
-                        "ip": Regex(valid_ip, error="you must provide an IP address that matches versions IPv4 or IPv6"),
-                        Optional("port"): And(int),
+                ),
+                "models": Schema(  # a sequence of mappings
+                    [{
+                        "name": And(str, len),
+                        "type": And(str, len),
+                        "inputs": And(dict, len, error="if 'inputs' is used,"
+                                      "it must contain at least one key-value "
+                                      "pair"),
+                        "outputs": And(dict, len, error="if 'outputs' is used,"
+                                       " it must contain at least one "
+                                       "key-value pair"),
+                        Optional("parameters"): And(dict,
+                                                    len,
+                                                    error="if 'parameters' is used, it must contain at least one key-value pair"),
+                        Optional("states"): And(dict, len, error="if 'states' is used, it must contain at least one key-value pair"),
+                        Optional("triggers"): And(list, len, error="if 'trigger' is used, it must contain at least one key-value pair"),
+                        Optional("scenario_data"): And(str, 
+                                                    Use(validate_file_path, error=f"'scenario_data' points to a file that doesn't exist"), 
+                                                    error="If 'scenario_data' is used, a valid file path must be provided"), 
+                        Optional("connect"): Schema( # a mapping of mappings
+                            {
+                                "ip": Regex(valid_ip, error="you must provide an IP address that matches versions IPv4 or IPv6"),
+                                Optional("port"): And(int),
                     }
                 ),
             } ]
