dc8a6661acce157d90a7ca53affbc72f1243fe0b
Manuel Garcia <gilbert.mg22@gmail.com>
2024-10-20 14:14:16 +0200

    Enhance validation functions in simulation.py for monitor connections and file paths

--- src/illuminator/schemas/simulation.py
+++ src/illuminator/schemas/simulation.py
@@ -19,7 +19,8 @@ valid_monitor_connection = r'^\w+\.\w+$'
 
 def validate_monitor_list(items: list) -> list:
     """
-    Validates the monitor section of the simulation configuration file.
+    Validates the monitor section of the simulation configuration file, as
+    follows the format <model>.<item>
     """
     pattern = re.compile(valid_monitor_connection)
     for item in items:
@@ -27,14 +28,13 @@ def validate_monitor_list(items: list) -> list:
             raise SchemaError(f"Invalid format for monitor item: {item}. Must be in the format: <model>.<item>")
     return items
 
-def validate_scenario_data_file(file_path: str) -> str:
+def validate_file_path(file_path: str) -> str:
     """
-    Validates the scenario data file exists.
+    Validates that a file path exists.
     """
-    if not os.path.isfile(file_path):
-        raise SchemaError(f"Scenario data file does not exist: {file_path}")
+    if not os.path.exists(file_path):
+        raise SchemaError(f"File path does not exist: {file_path}")
     return file_path
-
 
 class ScenarioSchema(Schema):
     """
@@ -70,7 +70,9 @@ schema = Schema( # a mapping of mappings
                 Optional("parameters"): And(dict, len, error="if 'parameters' is used, it must contain at least one key-value pair"),
                 Optional("states"): And(dict, len, error="if 'states' is used, it must contain at least one key-value pair"),
                 Optional("triggers"): And(list, len, error="if 'trigger' is used, it must contain at least one key-value pair"),
-                Optional("scenario_data"): And(str, len, Use(validate_scenario_data_file)),
+                Optional("scenario_data"): And(str, 
+                                               Use(validate_file_path, error=f"'scenario_data' points to a file that doesn't exist"), 
+                                               error="If 'scenario_data' is used, a valid file path must be provided"), 
                 Optional("connect"): Schema( # a mapping of mappings
                     {
                         "ip": Regex(valid_ip, error="you must provide an IP address that matches versions IPv4 or IPv6"),
@@ -85,7 +87,8 @@ schema = Schema( # a mapping of mappings
                 "to": Regex(valid_monitor_connection, error="Invalid format for 'to'. Must be in the format: <model>.<item>"),
             }]
         ),
-        "monitor":  And(list, len, Use(validate_monitor_list), error="you must provide at least one item to monitor"),
+        "monitor":  And(list, len, Use(validate_monitor_list, error="Items in 'monitor' must have the format: <model>.<item>"), 
+                        error="you must provide at least one item to monitor"),
     }
 )
 
