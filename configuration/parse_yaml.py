d227905fa6d81bcccc7a0f6306c66c170799dc00
Joan <j_pijpker@yahoo.com>
2024-10-22 09:07:03 +0200

    Added support for new definition of the connection

--- configuration/parse_yaml.py
+++ configuration/parse_yaml.py
@@ -1,5 +1,12 @@
 import yaml
 
+"""
+This script reads a yaml file of an illuminator model. It extracts the connection information.
+If an IP port is not specified it will default to 5123.
+If both IP address and port are known than a line will be prined that can be used in the run.sh file.
+The run.sh file is used to start the processes on remote pi's. 
+"""
+
 # Open and load the YAML file
 with open('modelling-example.yaml', 'r') as _file:
     data = yaml.safe_load(_file)
@@ -10,13 +17,15 @@ run_model='/home/illuminator/Desktop/Final_illuminator'
 for model in data['models']:
     model_type = model.get('type')
 
-    method = model.get('method', {})
-    method_type = method.get('type')
-    location = method.get('location')
-    connect_ip = method.get('connect_ip')
-    connect_port = method.get('connect_port')
+    connect = model.get('connect', {})
 
-    if method_type == 'connect':
+    if connect:
+        connect_ip = connect.get('ip')
+        connect_port = connect.get('port')
+
+        if not connect_port:
+            connect_port = 5123
+
         if connect_ip and connect_port:
             print(f"lxterminal -e ssh illuminator@{connect_ip} '{run_path}run{model_type}.sh {connect_ip} {connect_port} {run_model}'&")
 
