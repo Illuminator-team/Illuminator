29885573355f0b2c219ac32e3b989aef4f673d21
Manuel Garcia <gilbert.mg22@gmail.com>
2024-11-05 11:59:46 +0100

    fix issue with running unit tests

--- src/illuminator/models/__init__.py
+++ src/illuminator/models/__init__.py
@@ -2,9 +2,8 @@
 # In the future, a model will be contained in a single file, where the
 # name of the file matches the name of the model
 from .Battery.battery_model import BatteryModel
-from .adder import AdderModel
 from .collector import Collector
 from .mosaik_csv import CSV
 from .PV.pv_mosaik import PvAdapter
 
-__all__ = ['BatteryModel', 'AdderModel', 'Collector', 'CSV', 'PvAdapter']
+__all__ = ['BatteryModel', 'Collector', 'CSV', 'PvAdapter']
