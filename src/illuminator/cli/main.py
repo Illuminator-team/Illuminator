dd4cc0d0fdd084e9453ec56b0933eb1fe7be7c63
Manuel Garcia <gilbert.mg22@gmail.com>
2024-11-07 17:27:02 +0100

    Refactor scenario_run and cluster_build commands to use load_config_file and improve run.sh generation

--- src/illuminator/cli/main.py
+++ src/illuminator/cli/main.py
@@ -6,7 +6,15 @@ By: M. Rom & M. Garcia Alvarez
 import typer
 from typing_extensions import Annotated
 import illuminator.engine as engine
+from pathlib import Path
+from illuminator.cluster import build_runshfile
+from illuminator.schema.simulation import load_config_file
 
+APP_NAME = "illuminator"
+DEFAULT_PORT = 5123
+RUN_PATH = './Desktop/illuminatorclient/configuration/runshfile/'
+RUN_MODEL = '/home/illuminator/Desktop/Final_illuminator'
+RUN_FILE = 'run.sh'
 
 app = typer.Typer()
 scenario_app = typer.Typer()
@@ -14,12 +22,11 @@ app.add_typer(scenario_app, name="scenario", help="Run simulation scenarios.")
 cluster_app = typer.Typer()
 app.add_typer(cluster_app, name="cluster", help="Utilities for a RaspberryPi cluster.")
 
-
 @scenario_app.command("run")
-def scenario_run(config: Annotated[str, typer.Argument(help="Path to scenario configuration file.")] = "config.yaml"):
+def scenario_run(config_file: Annotated[str, typer.Argument(help="Path to scenario configuration file.")] = "config.yaml"):
     "Runs a simulation scenario using a YAML file."
 
-    file_path = config
+    file_path = config_file
 
     # load and validate configuration file
     config = engine.validate_config_data(file_path)
@@ -73,9 +80,19 @@ def scenario_run(config: Annotated[str, typer.Argument(help="Path to scenario co
 
 
 @cluster_app.command("build")
-def cluster_build(config: Annotated[str, typer.Argument(help="Path to scenario configuration file.")] = "config.yaml"):
+def cluster_build(config_file: Annotated[str, typer.Argument(help="Path to scenario configuration file.")] = "config.yaml"):
     """Builds the run.sh files for a cluster of Raspberry Pi's."""
-    print('building sh')
+    app_dir = typer.get_app_dir(APP_NAME)
+    runsh_path: Path = Path(app_dir) / "runshfile"
+
+    runsh_path.mkdir(parents=True, exist_ok=True)
+
+    output_file = RUN_FILE
+    data = load_config_file(config_file)
+    
+    if data:
+        build_runshfile.process_models(data, output_file)
+        print(f"Commands have been written to {output_file}")
 
 
 if __name__ == "__main__":
