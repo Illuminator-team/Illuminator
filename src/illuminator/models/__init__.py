702ecc8f6da9d1e48ef0e69522295e22d98874de
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2024-12-03 09:38:50 +0100

    battery model converted to version 3 structure. Example provided (battery.yaml)

--- src/illuminator/models/__init__.py
+++ src/illuminator/models/__init__.py
@@ -1,14 +1,15 @@
 # This is a temporary  fix to make models more accessible
 # In the future, a model will be contained in a single file, where the
 # name of the file matches the name of the model
-from .Battery.battery_model import BatteryModel
+#from .Battery.battery_model import BatteryModel
+from .Battery.battery_v3 import Battery
 from .collector import Collector
 # from .mosaik_csv import CSV
 from .CSV_reader_v3 import CSV
 from .PV.pv_mosaik import PvAdapter
 from .adder import Adder
 
-__all__ = ['BatteryModel', 
+__all__ = ['Battery', 
            'Collector', 
            'Adder', 
            'CSV', 
