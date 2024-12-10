9abafe1c092275aee526aae9723329253e5d8773
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2024-12-10 16:53:46 +0100

    default values for Load model

--- src/illuminator/models/Load/load_v3.py
+++ src/illuminator/models/Load/load_v3.py
@@ -21,6 +21,22 @@ load = IlluminatorModel(
 
 # construct the model
 class Load(ModelConstructor):
+    parameters={'houses': 1,  # number of houses that determine the total load demand
+                'output_type': 'power',  # type of output for consumption calculation ('energy' or 'power')
+                }
+    inputs={'load': 0}  # incoming energy or power demand per house (kWh) for each time step (15 minutes)
+    outputs={'load_dem': 0,  # total energy or power consumption for all houses (kWh) over the time step
+             'consumption': 0,  # Current energy or power consumption based on the number of houses and input load (kWh)
+            'time': None,  # Current simulation time step in seconds
+            'forecast': None  # Forecasted load demand (if applicable, not defined in the code but mentioned in META)
+             }
+    states={'consumption': 0,
+            'time': None,
+            'forecast': None
+            }
+    time_step_size=1
+    time=None
+
     def __init__(self, **kwargs) -> None:
         # TODO make a generalised way of doing this shit in the ModelConstructor __init__()
         super().__init__(**kwargs)
