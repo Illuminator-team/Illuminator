a6a394b50396f06420369724236deb74de117a9b
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-01-14 15:53:18 +0100

    load now follows pylint

--- src/illuminator/models/Load/load_v3.py
+++ src/illuminator/models/Load/load_v3.py
@@ -1,6 +1,20 @@
-from illuminator.builder import IlluminatorModel, ModelConstructor
+from illuminator.builder import ModelConstructor
 
 class Load(ModelConstructor):
+    """
+        Calculates total load demand based on number of houses and input load.
+
+        Parameters
+        ----------
+        load : float
+            Input load per house in kW or kWh depending on output_type
+            
+        Returns
+        -------
+        re_params : dict
+            Dictionary containing calculated load demand values
+        """
+
     parameters={'houses': 1,  # number of houses that determine the total load demand
                 'output_type': 'power',  # type of output for consumption calculation ('energy' or 'power')
                 }
@@ -16,13 +30,44 @@ class Load(ModelConstructor):
             }
     time_step_size=1
     time=None
-    
+
 
     def __init__(self, **kwargs) -> None:
+        """
+        Initialize Load model with given parameters.
+
+        Parameters
+        ----------
+        kwargs : dict
+            Dictionary containing model parameters and initial states
+            
+        Returns
+        -------
+        None
+        """
         super().__init__(**kwargs)
+        self.consumption = 0
 
 
-    def step(self, time, inputs, max_advance=1) -> None:
+    def step(self, time: int, inputs: dict=None, max_advance: int=900) -> None:
+        """
+        Performs a single simulation time step by calculating load demand.
+
+        Parameters
+        ----------
+        time : float
+            Current simulation time in seconds
+        inputs : dict
+            Dictionary containing input values including 'load'
+        max_advance : int, optional
+            Maximum time step advancement, defaults to 1
+            
+        Returns
+        -------
+        float
+            Next simulation time step
+        """
+
         input_data = self.unpack_inputs(inputs)
         self.time = time
 
@@ -31,23 +76,21 @@ class Load(ModelConstructor):
         self.set_outputs(results)
 
         return time + self._model.time_step_size
-    
+
 
     def demand(self, load:float) -> dict:
         """
-        Performs calculations of load and returns the selected parameters
-
-        ...
+        Calculates total load demand based on number of houses and input load.
 
         Parameters
         ----------
         load : float
-            Incoming load in kWh at every 15 min interval
+            Input load per house in kW or kWh depending on output_type
             
         Returns
         -------
         re_params : dict
-            Collection of parameters and their respective values
+            Dictionary containing calculated load demand values
         """
         # incoming load is in kWh at every 15 min interval
         # incoming value of load is in kWh
@@ -56,9 +99,9 @@ class Load(ModelConstructor):
         deltaTime = self.time_resolution * self.time_step_size / 60 / 60
 
         if output_type == 'energy':
-            self.consumption = (houses * load) # kW
+            self.consumption = houses * load # kW
         elif output_type == 'power':
-            self.consumption = (houses * load) * deltaTime # kWh
+            self.consumption = houses * load * deltaTime # kWh
 
         re_params = {'load_dem': self.consumption}
         return re_params
