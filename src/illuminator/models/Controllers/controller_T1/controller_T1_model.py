bbf622696824a03c31467777e9f15847334688e9
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-01-17 13:41:47 +0100

    added new controller model 'controller_T1', based on Tutorial of Jana

---
+++ src/illuminator/models/Controllers/controller_T1/controller_T1_model.py
@@ -0,0 +1,67 @@
+class Controller_ResLoad:
+    
+    # make adaptable with different combinations of RES generation and 
+    def __init__(self, soc_min = None,soc_max = None, max_p = None):
+        self.res_load = 0
+        if soc_min != None and soc_max != None:
+            self.soc_max_b = soc_max
+            self.soc_min_b = soc_min
+            self.max_p_b = max_p # maximum power of battery
+            self.bat_active = 1
+        else:
+            self.bat_active = 0
+
+        self.dump = 0
+        self.flow_b = 0  # flow_b is the flow to the battery so negative when discharge, positive when charge
+        #looking at the battery model the flow_b is the power flow
+
+    def control(self, wind_gen, pv_gen, load_dem, soc = None, load_EV = None):
+        #reset flow2b
+        self.flow_b = 0
+        self.soc_b = soc
+        print('Load dem: ' + str(load_dem))
+        print('pv: ' + str(pv_gen))
+        print('wind: ' + str(wind_gen))
+        if load_EV != None:
+            print('load EV: ' + str(load_EV))
+            self.res_load = load_dem + load_EV - wind_gen - pv_gen  # kW
+        else:
+            self.res_load = load_dem - wind_gen - pv_gen # kW
+
+        if self.bat_active == 1:
+            if self.res_load > 0:
+                # demand not satisfied -> discharge battery if possible
+                if self.soc_b > self.soc_min_b:  # checking if soc is above minimum
+                    print('Discharge Battery')
+                    max_discharge = (self.soc_b-self.soc_min_b)/100 * self.max_p_b
+                    print(f'max discharge: {max_discharge}')
+                    print(f'res load: {self.res_load}')
+                    self.flow_b = -min(self.res_load, max_discharge)
+                    print ('Flow Bat: '  + str(self.flow_b))
+                    #self.soc_b = self.soc_b + self.flow_b soc is not updated in controller
+                          
+            elif self.res_load < 0:
+            
+                if self.soc_b < self.soc_max_b:
+                    print('Charge Battery')
+                    max_flow2b = ((self.soc_max_b-self.soc_b)/100) * self.max_p_b  # Energy flow in kW
+                    self.flow_b = min((-self.res_load), max_flow2b)
+                    print ('Flow Bat: '  + str(self.flow_b))
+                    print('Excess generation that cannot be stored: ' + str(-self.res_load-self.flow_b))
+
+            else:
+                print('No Residual Load, RES production exactly covers demand')
+                self.flow_b = 0
+                self.dump = 0
+                #demand_res = residual_load
+
+            #update residual load with battery discharge/charge
+            #self.res_load = self.res_load + self.flow_b
+            self.dump = -(self.res_load + self.flow_b)
+        print('residual load: ' + str(self.res_load))
+        print('battery flow: ' + str(self.flow_b))
+        #if self.bat_active == 1:
+        re_params = {'flow2b': self.flow_b,'res_load': self.res_load,'dump': self.dump}
+        #else:
+            #re_params = {'res_load': self.res_load,'dump': self.dump}
+        return re_params
--- src/illuminator/models/__init__.py
+++ src/illuminator/models/__init__.py
@@ -10,14 +10,14 @@ from .CSV_reader_v3 import CSV
 from .PV.pv_model_v3 import PV
 from .adder import Adder
 from .Load.load_v3 import Load
-from .Controller.controller_v3 import Controller
+from .Controllers.default_controller.controller_v3 import Controller
+from .Controllers.controller_T1.controller_T1_v3 import Controller_T1
 from .Wind.wind_v3 import Wind
 
 __all__ = ['Battery', 
            'Collector', 
            'Adder', 
-           'CSV', 
-           'PvAdapter',
+           'CSV',
            'Load',
            'PV',
            'Controller',
