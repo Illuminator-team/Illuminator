f390e8c7478edf7299492a598c6a710dcd65b55f
lcklootwijk <l.c.klootwijk@student.nl>
2025-01-22 12:26:31 +0100

    Made more pylint compattible, fixed some errors but does not run yet (ScenrioError)

--- src/illuminator/models/Thermolyzer/thermolyzer_v3.py
+++ src/illuminator/models/Thermolyzer/thermolyzer_v3.py
@@ -1,25 +1,46 @@
 from illuminator.builder import ModelConstructor
 
 class Thermolyzer(ModelConstructor):
+    """
+    Thermolyzer model class for simulating a thermolyzer that converts biomass and electricity 
+    into hydrogen.
+
+    Parameters
+    ----------
+    eff : float
+        Thermolyzer efficiency as a percentage [%]
+    C_bio_h2 : float
+        Conversion factor from biomass to hydrogen [-]
+    C_CO2 : float
+        Absorption factor carbondioxide per h2 produced [-]
+    C_Eelec_h2 : float
+        Conversion factor electrical energy to hydrogen mass [kWh/kg]
+    max_ramp_up : float
+        Maximum ramp up in power per timestep [kW/timestep]
+    max_p_in : float
+        Maximum input power [kW]
+
+    Returns
+    -------
+    None
+    """
     parameters={
             'eff' : 70,             # thermolyzer effficiency [%]  
             'C_bio_h2' : 60,        # conversion factor from biomass to hydrogen [-] 
             'C_CO2' : 10,           # absorption factor carbondioxide per h2 produced [-] 
             'C_Eelec_h2': 11.5,     # conversion factor electrical energy to hydrogen mass [kWh/kg]    
-            'max_ramp_up' : 10      # maximum ramp up in power per timestep [kW/timestep]  
-            
-    },
+            'max_ramp_up' : 10,     # maximum ramp up in power per timestep [kW/timestep]  
+            'max_p_in' : 100        # maximum input power [kW]
+            },
     inputs={
             'biomass_in' : 0,       # biomass input [kg/timestep]
             'flow2t' : 0            # power input to the thermolyzer [kW]
             # 'water_in' : 0        # water input to the thermolzyer [L/timestep]
-
-    },
+            },
     outputs={
             'h_gen' : 0,            # hydrogen generation [kg/timestep]
             'CO2_out' : 0           # CO2 output [kg/timestep]
-
-    },
+            },
     states={},
 
     # other attributes
@@ -27,9 +48,64 @@ class Thermolyzer(ModelConstructor):
     time=None
     hhv =  286.6                # higher heating value of hydrogen [kJ/mol]
     mmh2 = 2.02                 # molar mass hydrogen (H2) [gram/mol]
-    
-    def step(self, time, inputs, max_advance=1) -> None:
-        
+
+
+    def __init__(self, **kwargs):
+        """
+        Initialize the thermolyzer model with specified parameters.
+
+        This method initializes a new thermolyzer instance with given parameters, setting up
+        initial values for efficiency, conversion factors, ramp limits, and other operational
+        parameters.
+
+        Parameters
+        ----------
+        kwargs : dict
+            Dictionary containing model parameters including:
+            - eff: Thermolyzer efficiency as a percentage [%]
+            - C_bio_h2: Conversion factor from biomass to hydrogen [-]
+            - C_CO2: Absorption factor carbondioxide per h2 produced [-]
+            - C_Eelec_h2: Conversion factor electrical energy to hydrogen mass [kWh/kg]
+            - max_ramp_up: Maximum ramp up in power per timestep [kW/timestep]
+            - max_p_in: Maximum input power [kW]
+
+        Returns
+        -------
+        None
+        """
+        super().__init__(**kwargs)
+        self.eff = self._model.parameters.get('eff')
+        self.C_bio_h2 = self._model.parameters.get('C_bio_h2')
+        self.C_CO2 = self._model.parameters.get('C_CO2')
+        self.C_Eelec_h2 = self._model.parameters.get('C_Eelec_h2')
+        self.max_ramp_up = self._model.parameters.get('max_ramp_up')
+        self.max_p_in = self._model.parameters.get('max_p_in')
+
+        self.p_in_last = 0  # Indicator of the last power input initialized to be 0 
+
+    def step(self, time: int, inputs: dict=None, max_advance: int=900) -> None:
+        """
+        Process one simulation step of the thermolyzer model.
+
+        This method processes the inputs for one timestep and sets the model outputs and states
+        accordingly.
+
+        Parameters
+        ----------
+        time : int
+            Current simulation time
+        inputs : dict
+            Dictionary containing input values, specifically:
+            - biomass_in: Biomass input in kg/timestep
+            - flow2t: Power input to the thermolyzer in kW
+        max_advance : int, optional
+            Maximum time step size in seconds (default 900)
+
+        Returns
+        -------
+        int
+            Next simulation time step
+        """
         print("\nThermolyzer:")
         print("inputs (passed): ", inputs)
         print("inputs (internal): ", self._model.inputs)
@@ -50,7 +126,6 @@ class Thermolyzer(ModelConstructor):
         self._model.outputs['h_gen'] = h_gen
         self._model.outputs['CO2_out'] = CO2_out
         print("outputs:", self.outputs)
-        
         return time + self._model.time_step_size
     
     def ramp_lim(self, flow2t, max_advance):
@@ -71,13 +146,12 @@ class Thermolyzer(ModelConstructor):
         """
         # restrict the power input to not increase more than max_p_ramp_rate
         # compared to the last timestep
-        # TODO: check method of using paramters (self. or .get())
         p_change = flow2t - self.p_in_last
-        if abs(p_change) > self.max_p_ramp_rate:
+        if abs(p_change) > self.max_ramp_up:
             if p_change > 0:
-                power_in = self.p_in_last + self.max_p_ramp_rate * max_advance
+                power_in = self.p_in_last + self.max_ramp_up * max_advance
             else:
-                power_in = self.p_in_last - self.max_p_ramp_rate * max_advance
+                power_in = self.p_in_last - self.max_ramp_up * max_advance
         else:
             power_in = flow2t
         self.p_in_last = power_in
