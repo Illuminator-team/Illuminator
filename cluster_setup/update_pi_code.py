1b1503bc839aa819bde4c6ecc44f135be4d09c14
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-03-19 14:55:21 +0100

    new script to update execute 'git pull' on all pi's

---
+++ cluster_setup/update_pi_code.py
@@ -0,0 +1,15 @@
+import subprocess
+from os import system, getcwd
+
+USERNAME = 'Raspinator'
+
+ips = ['192.168.27.19', '192.168.27.161']
+
+print('updating master')
+system('git pull')
+
+for client_ip in ips:
+    print('updating Client: ', client_ip)
+    subprocess.run(f"ssh {USERNAME}@{client_ip} 'git pull'", shell=True)
+
+print("\nDONE")
