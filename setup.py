9755dad04e9a900541b585d44fb8f46dd4172e4e
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-03-19 14:40:44 +0100

    setup python script now works and tested! ran from master to configure clients

--- setup.py
+++ setup.py
@@ -1,19 +1,49 @@
-from os import system
+import subprocess
+from os import system, getcwd
 
 USERNAME = 'Raspinator'
 
+client_ip = input('please provide the ip of the Client pi: ')
+# password = input('please provide the password of the Client pi: ')
+
+# def remote_command(command: str):
+#     # Run SSH with interactive input for the password
+#     proc = subprocess.Popen(
+#         ["ssh", f"{USERNAME}@{client_ip}", command],
+#         stdin=subprocess.PIPE,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#         text=True
+#     )
+
+#     # Send the password when prompted
+#     proc.stdin.write(password + "\n")
+#     proc.stdin.flush()
+
+#     # Capture and print output
+#     stdout, stderr = proc.communicate()
+#     print("stdout::\n", stdout)
+#     print("\nstderr:\n", stderr)
+
+# generate ssh keys and send them to the slave
+print('generating ssh keys and sending it to the new Client')
+# print('current directory: ', getcwd())
+system(f"python cluster_setup/generate_ssh_key.py {USERNAME} {client_ip}")
+
 # setting static ip
-print('making ip adres static')
-system('python cluster_setup/static_ip.py')
+print('\n\n##########################################')
+print('making ip address static')
+subprocess.run(f"ssh {USERNAME}@{client_ip} 'sudo python Illuminator/cluster_setup/static_ip.py'", shell=True)
+# system('python cluster_setup/static_ip.py')
 
 # give executive permission to all files in runshfile/
+print('\n\n##########################################')
 print('making runshfiles executable')
-system('chmod +x configuration/runshfile/*.sh')
+subprocess.run(f"ssh {USERNAME}@{client_ip} 'chmod +x Illuminator/configuration/runshfile/*.sh'", shell=True)
 
-# generate ssh keys and send them to the master
-print('generating ssh keys and sending it to the master')
-master_ip = input('please provide the ip of the master pi: ')
-system(f"cluster_setup/generate_ssh_key.py {USERNAME} {master_ip}")
 
-print('FINISHED')
+print('\n\n##########################################')
+print('setup finished, rebooting new Client')
+subprocess.run(f"ssh {USERNAME}@{client_ip} 'sudo reboot'", shell=True)
 
+print("\nDONE")
