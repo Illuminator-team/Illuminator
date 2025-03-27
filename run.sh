b1c7caa7eb56feb31532e874148ac50384c1eea8
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-03-27 15:07:03 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-lxterminal -e ssh Raspinator@192.168.27.161 './Illuminator/configuration/runshfile/runPV.sh 192.168.27.161 5123 /home/Raspinator/Illuminator/src/illuminator/models/'&
-lxterminal -e ssh Raspinator@192.168.27.161 './Illuminator/configuration/runshfile/runLED_connection.sh 192.168.27.161 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
-lxterminal -e ssh Raspinator@192.168.27.19 './Illuminator/configuration/runshfile/runWind.sh 192.168.27.19 5001 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.27.19 './Illuminator/configuration/runshfile/runWind.sh 192.168.27.19 5123 /home/Raspinator/Illuminator/src/illuminator/models/'&
 lxterminal -e ssh Raspinator@192.168.27.19 './Illuminator/configuration/runshfile/runLED_connection.sh 192.168.27.19 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
