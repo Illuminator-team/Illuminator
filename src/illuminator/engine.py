35813c1ce9530403011cca93ef110bd3811e667b
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-05-13 16:00:28 +0200

    Monitor now organizes its columns according to as defined in the yaml file

--- src/illuminator/engine.py
+++ src/illuminator/engine.py
@@ -459,7 +459,8 @@ class Simulation:
         # A way to define custom collectors should be provided by the Illuminator.
         collector = world.start('Collector', 
                                 time_resolution=_time_resolution, 
-                                start_date=_start_time,  
+                                start_date=_start_time,
+                                items = config['monitor']['items'],  
                                 results_show={'write2csv':True, 'dashboard_show':False, 
                                             'Finalresults_show':False,'database':False, 'mqtt':False}, 
                                 output_file=_results_file)
