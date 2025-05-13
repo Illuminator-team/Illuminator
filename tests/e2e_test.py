0438b4045e71e13dcb6311b0ca9396ed0a2d73f2
Jort Groen <7791955+JortGroen@users.noreply.github.com>
2025-05-13 14:10:30 +0200

    the workingdir is the repo

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -1,13 +1,13 @@
 from illuminator.engine import Simulation
 
 # initialize simulation according to tutorial1 yaml
-CONFIG_FILE = 'Illuminator/tests/data/Tutorial_1.yaml'
+CONFIG_FILE = 'tests/data/Tutorial_1.yaml'
 simulation = Simulation(CONFIG_FILE)
-simulation.set_model_param(model_name='CSVload', parameter='file_path', value='Illuminator/tests/data/load_data.txt')
-simulation.set_model_param(model_name='CSV_pv', parameter='file_path', value='Illuminator/tests/data/pv_data_Rotterdam_NL-15min.txt')
-simulation.set_model_param(model_name='CSV_wind', parameter='file_path', value='Illuminator/tests/data/winddata_NL.txt')
+simulation.set_model_param(model_name='CSVload', parameter='file_path', value='tests/data/load_data.txt')
+simulation.set_model_param(model_name='CSV_pv', parameter='file_path', value='tests/data/pv_data_Rotterdam_NL-15min.txt')
+simulation.set_model_param(model_name='CSV_wind', parameter='file_path', value='tests/data/winddata_NL.txt')
 
-simulation.set_monitor_param(parameter='file', value='Illuminator/tests/outputs/out_e2e_T1.csv')
+simulation.set_monitor_param(parameter='file', value='tests/outputs/out_e2e_T1.csv')
 
 # # set multiple models at once
 # new_settings = {'Wind1': {'diameter': 5},
