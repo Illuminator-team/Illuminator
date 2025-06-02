26130efc7b9f36332bb163cbb265ba3c918c502d
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-06-02 13:54:03 +0200

    adding hydrogen models to the init

--- src/illuminator/models/__init__.py
+++ src/illuminator/models/__init__.py
@@ -18,10 +18,25 @@ from .Load.LoadHeatpump.load_heatpump_v3 import LoadHeatpump
 
 from .Battery.battery_v3 import Battery
 
+# Hydrogen models
+from .Thermolyzer.thermolyzer_v3 import Thermolyzer
+from .Compressor.compressor_v3 import Compressor
+from .H2demand.h2demand_v3 import H2demand
+from .H2pipeline.h2pipeline import Pipeline
+from .H2storage.h2storage_v3 import H2Storage
+from .H2valve.h2valve_v3 import H2Valve
+from .H2_joint.H2_joint_v3 import H2Joint
+from .H2controller.H2_controller_v3 import H2Controller
+from .H2controller.H2_controller2 import H2Controller2
+from .Buffer.buffer import H2Buffer
+from .H2controller.H2_controller_example import H2ControllerExample
+
+# controllers
 from .Controllers.default_controller.controller_v3 import Controller
 from .Controllers.controller_T1.controller_T1_v3 import Controller_T1
 from .Controllers.controller_T3Congestion.controller_T3Congestion_v3 import ControllerT3Congestion
 
+# agents
 from .Agents.generators.generation_company_agent_v3 import GenerationCompanyAgent
 from .Agents.operators.operator_v3 import Operator_Market
 from .Agents.justice_agent.justice_agent_v3 import JusticeAgent
@@ -36,6 +51,17 @@ __all__ = [ 'Adder',
             'LoadEV',
             'LoadHeatpump',
             'Battery', 
+            'Thermolyzer',
+            'Compressor',
+            'H2demand',
+            'Pipeline',
+            'H2Storage',
+            'H2Valve',
+            'H2Joint',
+            'H2Controller',
+            'H2Controller2',
+            'H2ControllerExample',
+            'H2Buffer',
             'Controller',
             'Controller_T1',
             'ControllerT3Congestion',
