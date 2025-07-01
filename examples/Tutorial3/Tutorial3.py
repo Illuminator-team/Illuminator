e64ca2b31097aa257f9179aa5df83a8edc8b0df3
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-07-01 14:57:51 +0200

    Tutorial3.py now works from the editor

--- examples/Tutorial3/Tutorial3.py
+++ examples/Tutorial3/Tutorial3.py
@@ -1,25 +1,21 @@
-from os import chdir, getcwd
 from illuminator.engine import Simulation
-cwd = getcwd()
-
-chdir('Illuminator/examples/Tutorial3')  # working directory (path to the folder containing the tutorial files)
 
 # initialize simulation according to tutorial1 yaml
-CONFIG_FILE = 'Tutorial_3.yaml'
+CONFIG_FILE = 'examples/Tutorial3/Tutorial_physical_congestion_RES_bat_elec_loadshift.yaml'
 simulation = Simulation(CONFIG_FILE)
 
-# write the output to a file
-# OUT_FILE = 'out_Tutorial3.csv'
-# simulation.set_model_param(model_name='monitor', parameter='file', value=OUT_FILE)
+simulation.set_model_param(model_name='CSVload', parameter='file_path', value='examples/Tutorial3/data/load_data.txt')
+simulation.set_model_param(model_name='CSVloadEV', parameter='file_path', value='examples/Tutorial3/data/10_EVs_2012_synthetic.csv')
+simulation.set_model_param(model_name='CSVloadHP', parameter='file_path', value='examples/Tutorial3/data/hp_profiles_10h_kW.csv')
+simulation.set_model_param(model_name='CSV_pv', parameter='file_path', value='examples/Tutorial3/data/pv_data_Rotterdam_NL-15min.txt')
+simulation.set_model_param(model_name='CSV_wind', parameter='file_path', value='examples/Tutorial3/data/winddata_NL.txt')
 
-# set multiple models at once
-new_settings = {'Wind1': {'diameter': 5},
-                'Load1': {'houses': 100}
-                }
-simulation.edit_models(new_settings)
+
+
+# write the output to a file
+OUT_FILE = 'examples/Tutorial3/out_Tutorial3.csv'
+simulation.set_monitor_param('file', value=OUT_FILE)
 
 # run the simulation
 simulation.run()
-
-chdir(cwd)  # change back to the original working directory
 
