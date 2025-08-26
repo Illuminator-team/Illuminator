dcfffdfa3c8571108be40915ee5ceb26ccce7d98
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-08-26 13:55:11 +0200

    added initial commit for story controller for managing the phases of a story (switching of csv's)

--- tests/e2e_story.py
+++ tests/e2e_story.py
@@ -1,13 +1,13 @@
 from illuminator.engine import Simulation
 
 # initialize simulation according to tutorial1 yaml
-CONFIG_FILE = 'tests/data/Tutorial_1.yaml'
+CONFIG_FILE = 'tests/data/Tutorial_story.yaml'
 simulation = Simulation(CONFIG_FILE)
-simulation.set_model_param(model_name='CSV_pv', parameter='file_path', value='examples/Tutorial1/pv_data_Rotterdam_NL-15min.txt')
-simulation.set_model_param(model_name='CSV_wind', parameter='file_path', value='examples/Tutorial1/winddata_NL.txt')
+# simulation.set_model_param(model_name='CSV_pv', parameter='file_path', value='examples/Tutorial1/pv_data_Rotterdam_NL-15min.txt')
+# simulation.set_model_param(model_name='CSV_wind', parameter='file_path', value='examples/Tutorial1/winddata_NL.txt')
 
-csv_files = ['examples/Tutorial1/load_data.txt', 'examples/Tutorial1/load_data_2.txt']
-simulation.set_model_param(model_name='CSVload', parameter='file_path', value=csv_files)
+# csv_files = ['examples/Tutorial1/load_data.txt', 'examples/Tutorial1/load_data_2.txt']
+# simulation.set_model_param(model_name='CSVload', parameter='file_path', value=csv_files)
 
 
 
