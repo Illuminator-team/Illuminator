50ccd090c78a3861a94e3b6611346b39bef3a6af
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-09-17 15:48:30 +0200

    add 2nd pi

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-lxterminal -e ssh Raspinator@192.168.27.19 './Illuminator/configuration/runshfile/runWind.sh 192.168.27.19 5123 /home/Raspinator/Illuminator/src/illuminator/models/'&
-lxterminal -e ssh Raspinator@192.168.27.19 './Illuminator/configuration/runshfile/runLED_connection.sh 192.168.27.19 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@10.89.147.122 './Illuminator/configuration/runshfile/runLED_connection.sh 10.89.147.122 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
