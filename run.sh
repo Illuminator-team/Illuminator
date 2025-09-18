5030d396cb0d1454d0b98c7e636f3ea0796fd1f8
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-09-18 16:17:03 +0200

    activate virtual env before running

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-lxterminal -e ssh Raspinator@10.89.147.122 './Illuminator/configuration/runshfile/runLED_connection.sh 10.89.147.122 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@10.89.147.122 'source .venvs/ill-cluster/bin/activate; ./Illuminator/configuration/runshfile/runLED_connection.sh 10.89.147.122 5124 /home/Raspinator/Illuminator/src/illuminator/models/'&
