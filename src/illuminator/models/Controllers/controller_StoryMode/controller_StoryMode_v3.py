13a38edd3b0084cecc3ae868d3176d83f1edbf6e
JortGroen <7791955+JortGroen@users.noreply.github.com>
2025-09-26 11:39:45 +0200

    check which connections are made, establish the mappings to the LED strip ids and define which story phase it is in

--- src/illuminator/models/Controllers/controller_StoryMode/controller_StoryMode_v3.py
+++ src/illuminator/models/Controllers/controller_StoryMode/controller_StoryMode_v3.py
@@ -1,5 +1,12 @@
 from illuminator.builder import ModelConstructor
 
+def check_connected(model_name1, model_name2, connections, ids):
+    for i, conn in enumerate(connections):
+        if conn == (model_name1, model_name2) or conn == (model_name2, model_name1):
+            return True, ids[i]
+    return False, None
+        
+
 # construct the model
 class Controller_StoryMode(ModelConstructor):
     """
@@ -64,18 +71,29 @@ class Controller_StoryMode(ModelConstructor):
         input_data = self.unpack_inputs(inputs, return_sources=True)  # make input data easily accessible
         self.time = time
 
-        connections = self.determine_connectivity(input_data['physical_connections'])
+        connections, ids = self.determine_connectivity(input_data['physical_connections'])
 
-        if ('PV_LED-0.time-based_0', 'Wind_LED-0.time-based_0') in connections or ('Wind_LED-0.time-based_0', 'PV_LED-0.time-based_0') in connections:
-            print("PV and Wind connected")
+        to_EWI_LED = []
+        to_Ext_LED = []
+        to_house_LED = []
+
+        story_phase = 0
+        is_connected1, conn_id1 = check_connected('EWI_LED-0.time-based_0', 'Ext_LED-0.time-based_0', connections, ids)
+        is_connected2, conn_id2 = check_connected('house_LED-0.time-based_0', 'Ext_LED-0.time-based_0', connections, ids)
+        if is_connected1 and not is_connected2:
+            print("EWI and Ext connected")
+            print("House and Ext connected")
+            story_phase = 1
+            to_house_LED.append({'other_side': 'Ext_LED-0.time-based_0', 'connection_id': conn_id1, 'direction': 1})
+            to_EWI_LED.append({'other_side': 'Ext_LED-0.time-based_0', 'connection_id': conn_id2, 'direction': 1})
+            to_Ext_LED.append({'other_side': 'house_LED-0.time-based_0', 'connection_id': conn_id1, 'direction': -1})
+            to_Ext_LED.append({'other_side': 'house_LED-0.time-based_0', 'connection_id': conn_id1, 'direction': -1})
+
             self.file_indeces['file_index_Load'] = 1
-        # elif time > 10:
-        #     self.file_indeces['file_index_Load'] = 1
-        else:
-            print("PV and Wind NOT connected")
-            self.file_indeces['file_index_Load'] = 0
+
 
         self.set_states(self.file_indeces)
+        self.set_states({'EWI_LED_mapping': to_EWI_LED, 'Ext_LED_mapping': to_Ext_LED, 'house_LED_mapping': to_house_LED})
 
         # return the time of the next step (time untill current information is valid)
         return time + self._model.time_step_size
@@ -97,6 +115,7 @@ class Controller_StoryMode(ModelConstructor):
         list of connections
         """
         connections = []
+        ids = []
         for i, (id1, model1) in enumerate(zip(physical_connections['value'], physical_connections['sources'])):
             for id2, model2 in zip(physical_connections['value'][i+1:], physical_connections['sources'][i+1:]):
                         # remove -1 before comparing
@@ -107,6 +126,7 @@ class Controller_StoryMode(ModelConstructor):
                 common_ids = list(set1 & set2)
                 if len(common_ids) > 0 and model1 != model2:
                     connections.append((model1, model2))
+                    ids.append(common_ids[0])
         print("\n\nconnections: ", connections, "\n\n")
-        return connections
+        return connections, ids
         
