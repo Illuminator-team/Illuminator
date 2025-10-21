e4a0f5e3e9763629e527d2a6f1f93eb4a0dfa6bc
Yasel Quintero <y.i.quinterolares@tudelft.nl>
2025-10-21 18:52:44 +0200

    Save parallel version of Tutorial4 in a different file
    Revert changes to src/illuminator/cli/main.py

--- src/illuminator/cli/main.py
+++ src/illuminator/cli/main.py
@@ -61,5 +61,6 @@ if __name__ == "__main__":
 
     # package_spec = importlib.util.find_spec("illuminator.models.Battery")
     # print(package_spec.origin)
-    scenario_run_parallel("examples/Tutorial4/Tutorial4.yaml") # Can be removed. its just to run the code programatically.
-    # app()
+
+
+    app()
