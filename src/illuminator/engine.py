cca945909ab103f8fc5c71b926d3e7bc6b207687
JortGroen <7791955+JortGroen@users.noreply.github.com>
2026-01-12 15:03:14 +0100

    came up with a solution that works on both windows and linux

--- src/illuminator/engine.py
+++ src/illuminator/engine.py
@@ -118,7 +118,7 @@ def generate_mosaik_configuration(config_simulation:dict,  collector:str =None) 
     # print(default_collector)
 
     if collector is None:
-        _collector = '%(python)s ' + default_collector.replace('\\', '/') + ' %(addr)s'
+        _collector = '"%(python)s" "{}" %(addr)s'.format(default_collector.replace('\\', '/'))
     else:
         _collector = collector
 
